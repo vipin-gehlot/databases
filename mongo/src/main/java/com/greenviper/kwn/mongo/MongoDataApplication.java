802396f6bff82398a202a6b3cbb040a755b8158f
Vipin <vipin.gehlot@ishisystems.com>
2016-07-08 16:12:26 +0530

    Initial commit - Project structure

---
+++ mongo/src/main/java/com/greenviper/kwn/mongo/MongoDataApplication.java
@@ -0,0 +1,12 @@
+package com.greenviper.kwn.mongo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDataApplication.class, args);
+    }
+}
